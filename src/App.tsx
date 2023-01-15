01c66f1117de01f826d70c3dfb7538cdb9405fea
Mohamed <mhassankd35@gmail.com>
2023-01-15 17:26:46 -0600

    creates dark mode logic handler

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,18 @@ import { useState, useEffect } from "react";
 import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
+  const [theme, setTheme] = useState("dark");
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
   return (
-    <div className="dark">
+    <div className="flex h-screen w-screen dark:bg-black">
       <Sidebar />
     </div>
   );
