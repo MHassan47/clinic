f524254db2637befa5a5c97bed84a7d8fc3b3b38
Mohamed <mhassankd35@gmail.com>
2023-01-16 13:04:40 -0600

    changes to flexbox sizing

--- src/components/Right/Chat.tsx
+++ src/components/Right/Chat.tsx
@@ -3,27 +3,27 @@ import { IoIosSend } from "react-icons/io";
 function Chat() {
   const [message, setMessage] = useState("");
   return (
-    <div className="flex flex-col justify-around w-2/3 h-2/5 shadow-2xl bg-gray-100 dark:bg-neutral-900 rounded-2xl dark:text-white transition duration-500 ease-in-out transform">
+    <div className="flex flex-col justify-around w-4/5 h-2/5 shadow-2xl bg-gray-100 dark:bg-neutral-900 rounded-2xl text-slate-700 transition duration-500 ease-in-out transform">
       <div className="flex justify-between items-center border-b-2 border-gray-400 dark:border-gray-500">
-        <h1 className="text-2xl font-bold  mx-6 mb-4"> Chat</h1>
+        <h1 className="text-2xl  font-bold  mx-6 mb-4"> Chat</h1>
         <div className="w-6 h-6 bg-black rounded-3xl  mx-6 mb-4" />
       </div>
       <div className="flex flex-col items-center gap-10 mt-6 mx-6">
         <div className="flex items-center ">
           <div className="flex w-5/6 items-end gap-4">
             <div className="w-6 h-6 bg-black rounded-3xl" />
-            <div className="flex h-max w-4/5 rounded-xl bg-darkgr p-4 font-medium">
+            <div className="flex h-max w-4/5 rounded-xl bg-darkgr p-4 font-medium text-white">
               {" "}
-              Hey, Sarah! How can I help you?{" "}
+              Hey, Dr. Kamal! How can I help you?
             </div>
           </div>
-          <h1 className="font-light ">6:02 pm</h1>
+          <h1 className="font-light dark:text-white">6:02 pm</h1>
         </div>
 
         <div className="flex items-center ">
-          <h1 className="font-light">6:02 pm</h1>
+          <h1 className="font-light dark:text-white">6:02 pm</h1>
           <div className="flex justify-end items-end w-5/6 gap-4">
-            <div className="flex h-max w-4/5 rounded-xl bg-gr p-4 font-medium">
+            <div className="flex h-max w-4/5 rounded-xl bg-gr p-4 font-medium text-white">
               {" "}
               Hey, How do I add a new patient?{" "}
             </div>
