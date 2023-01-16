7cc895a42b0c4ad2c315b2b07613ef30e4571422
Mohamed <mhassankd35@gmail.com>
2023-01-16 07:38:41 -0600

    small changes to opacity in dark mode

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -18,30 +18,30 @@ const Sidebar: FC = () => {
             className="flex items-center cursor-pointer mb-10"
             // onClick={() => navigate("/home")}
           >
-            <RxDashboard className="text-3xl mr-4 text-white dark:text-gray-400  opacity-60 hover:opacity-100 dark:hover:text-white hover:scale-110" />
+            <RxDashboard className="text-3xl mr-4 text-white dark:text-gray-400  opacity-60 dark:opacity-80 hover:opacity-100 dark:hover:text-white hover:scale-110" />
           </div>
           <div className="flex items-center mt-4 hover:text-blue-600 cursor-pointer mb-10">
-            <FaClipboardList className="text-3xl mr-4 text-white dark:text-gray-400 opacity-60 hover:opacity-100 dark:hover:text-white hover:scale-110" />
+            <FaClipboardList className="text-3xl mr-4 text-white dark:text-gray-400 opacity-60 dark:opacity-80 hover:opacity-100 dark:hover:text-white hover:scale-110" />
           </div>
           <div className="flex items-center mt-4 cursor-pointer mb-10">
-            <HiChatBubbleLeftRight className="text-3xl mr-4  text-white dark:text-gray-400 opacity-60 hover:opacity-100 dark:hover:text-white hover:scale-110 " />
+            <HiChatBubbleLeftRight className="text-3xl mr-4  text-white dark:text-gray-400 opacity-60 dark:opacity-80 hover:opacity-100 dark:hover:text-white hover:scale-110 " />
           </div>
           <div
             className="flex items-center mt-4 cursor-pointer mb-10"
             // onClick={() => navigate("/create")}
           >
-            <BsBarChartFill className="text-3xl mr-4 text-white dark:text-gray-400 opacity-60 hover:opacity-100 dark:hover:text-white hover:scale-110" />
+            <BsBarChartFill className="text-3xl mr-4 text-white dark:text-gray-400 opacity-60 dark:opacity-80 hover:opacity-100 dark:hover:text-white hover:scale-110" />
           </div>
         </div>
         <div className="flex flex-col px-8 py-2">
           <div className="flex items-center mt-4 cursor-pointer mb-10">
-            <HiUserAdd className="text-3xl mr-4 text-white dark:text-gray-400 opacity-60 hover:opacity-100 dark:hover:text-white hover:scale-110" />
+            <HiUserAdd className="text-3xl mr-4 text-white dark:text-gray-400 opacity-60 dark:opacity-80 hover:opacity-100 dark:hover:text-white hover:scale-110" />
           </div>
           <div className="flex items-center mt-4 cursor-pointer mb-10">
-            <MdSettings className="text-3xl mr-4 text-white dark:text-gray-400 opacity-60 hover:opacity-100 dark:hover:text-white hover:scale-110" />
+            <MdSettings className="text-3xl mr-4 text-white dark:text-gray-400 opacity-60 dark:opacity-80 hover:opacity-100 dark:hover:text-white hover:scale-110" />
           </div>
           <div className="flex items-center mt-4 cursor-pointer mb-10">
-            <HiLogout className="text-3xl mr-4 text-white dark:text-gray-400 opacity-60 hover:opacity-100 dark:hover:text-white hover:scale-110" />
+            <HiLogout className="text-3xl mr-4 text-white dark:text-gray-400 opacity-60 dark:opacity-80 hover:opacity-100 dark:hover:text-white hover:scale-110" />
           </div>
         </div>
       </div>
