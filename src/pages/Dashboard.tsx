671662e585606ab6e40f326280b133e4f55f5ace
Mohamed <mhassankd35@gmail.com>
2023-01-16 06:52:40 -0600

    handles theme props being passed down

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { FC } from "react";
 import DashboardContent from "../components/Dashboard/DashboardContent";
 import BaseLayout from "../components/Other/BaseLayout";
 
-function Dashboard() {
+interface props {
+  setTheme: React.Dispatch<React.SetStateAction<string>>;
+  theme: string;
+}
+const Dashboard: FC<props> = ({ setTheme, theme }) => {
   return (
     <div className="flex w-full">
       <BaseLayout>
-        <DashboardContent />
+        <DashboardContent setTheme={setTheme} theme={theme} />
       </BaseLayout>
     </div>
   );
-}
+};
 
 export default Dashboard;
